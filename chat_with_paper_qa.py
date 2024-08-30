d836962992b5bc0e27191999904a16779d1b57c9
monaal <monaalsanghvi1998@gmail.com>
2024-08-30 15:09:21 +0530

    fix bug while downloading pdfs

--- chat_with_paper_qa.py
+++ chat_with_paper_qa.py
@@ -1,6 +1,10 @@
+import io
+
 from paperqa import Docs
 import re
 import os
+
+from extract_figures import download_pdf
 
 os.environ['OPENAI_API_KEY'] = 'sk-None-IEWqcLkHft3djH66AroZT3BlbkFJ4cCc3ieleT5ArR1On5rK'
 
@@ -20,9 +24,20 @@ def extract_page_references(text):
     return matches
 
 def get_answer_from_paperqa(doc_urls, query):
+    """current_path = os.getcwd()
+    file_paths = []
+    for i in doc_urls:
+        pdf_name = f"downloaded_{i+1}.pdf"
+        response = download_pdf(i)
+    # Construct the full path for the PDF file
+        file_path = os.path.join(current_path, pdf_name)
+    # Write the content to a file
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        file_paths.append(file_path)"""
     docs = Docs(llm='gpt-4o-mini-2024-07-18', summary_llm="gpt-4o-mini-2024-07-18")
     for doc_url in doc_urls:
-        docs.add_url(doc_url)
+        docs.add_file(io.BytesIO(download_pdf(doc_url)))
     response = docs.query(query)
     return response.answer
 
