bb755c73f67e29b18d1e991432a94522db078fbf
monaal <monaalsanghvi1998@gmail.com>
2024-09-03 17:58:08 +0530

    change port for flask app

--- main.py
+++ main.py
@@ -115,5 +115,5 @@ def get_referenced_paper_info():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
