06b90af2b43ba2ac80ed2a2db8d271b85a82401e
monaal <monaalsanghvi1998@gmail.com>
2024-11-18 18:24:01 +0530

    update askQuestion prompts

--- main/classes/answer_a_question_classes.py
+++ main/classes/answer_a_question_classes.py
@@ -5,21 +5,14 @@ from classes.research_paper import ResearchPaper
 
 class Answer(BaseModel):
     reference_ids: List[str] = Field(
-        description="""List of reference_ids in the order that they appear in the answer. If there is a "[1]" in the answer, and that corresponds to first reference in the given list of references, extract the reference_id and add it here. """)
-    answer: str = Field(description="""Answer to the user question with inline citations like [1], [2] etc.  When writing your answers, reference these quotes using inline citations in square brackets [1], [2], etc. Each 
-                    statement in your answers should end with the relevant citation(s). Multiple citations can be used if needed [1,2].
-                    \n \nIf there is no relevant reference, write "No relevant Info" instead. \n \n Do not include or reference 
-                    quoted content verbatim in the answer. Make sure each statement in your answers includes at least one citation to the 
-                    relevant quote number. Don\'t say "According to Quote [1]"  or " Based on the information provided" when answering. 
-                    If the question cannot be answered by the document, say so.
-                    Make references to quotes relevant to each section of the answer solely by adding their "
-                     "bracketed ids at the end of relevant sentences. "
-                     """)
+        description="""List of reference_ids used in the answer""")
+    answer: str = Field(description="""Answer to the user question with inline citations like [W3155739706_32], 
+    [W3155739706_31] etc.""")
 
 
 class AnswerReference(BaseModel):
     reference_text: str
-    chunk_id: str
+    reference_id: str
     paper: ResearchPaper
 
     class Config:
--- main/prompts/answer_a_question_prompts.py
+++ main/prompts/answer_a_question_prompts.py
@@ -3,12 +3,15 @@ ANSWER_A_QUESTION_PROMPT = """You are an expert scientific research assistant. Y
                    They need a specific and highly technical answer to their question. Don't let your answer be a generic
                     one or vague. Respond Like you are a accomplished researcher. To answer the question, only use the 
                     references provided to you. 
-                    The format of the references will be : 
-
-                    A list of dictionaries, with each dictionary containing the following format : 
-                    - id - The id of the reference
-                    - text - The actual text of the reference
-
+                      When writing your answers, reference the references given to you by the user by adding the 
+                      reference_id using inline citations in square brackets [W3155739706_32], [W3155739706_31] etc. Each 
+                    statement in your answers should end with the relevant citation(s). Multiple citations can be used if needed [W3155739706_32, W3155739706_31].
+                    \n \nIf there is no relevant reference, write "No relevant Info" instead. \n \n Do not include or reference 
+                    quoted content verbatim in the answer. Make sure each statement in your answers includes at least one citation to the 
+                    relevant quote number. Don\'t say "According to Quote [W3155739706_31]"  or " Based on the information provided" when answering. 
+                    If the question cannot be answered by the document, say so.
+                    Make references to quotes relevant to each section of the answer solely by adding their "
+                     "bracketed ids at the end of relevant sentences. "
 
                      Here is the question from the user : {question}
                      Here is the list of references : {reference_list}
